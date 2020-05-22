2c6d90e26b22b2b1c44ae911060ee7bf842e4f84
Daniel Grace <Daniel.grace200011@gmail.com>
2020-05-22 17:37:37 +0100

    Optional task complete, added payments

---
+++ CO453_WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CO453_WebApplication1.Models;
+
+namespace CO453_WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Customer()
+        {
+            Customer customer = new Customer
+            {
+                CustomerID = 1,
+                FullName = "Daniel",
+                Address = "Hp430op",
+                Email = "danielgrace@gmail.com",
+                PhoneNumber = "061234 567 345"
+            };
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Customer(Customer customer)
+        {
+            //Save to database
+            ViewData["Customer"] = customer;
+            ViewBag.Customer = customer;
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult PaymentDetails()
+        {
+            PaymentDetails payment = new PaymentDetails
+            {
+                FirstName = "Daniel",
+                SecondName = "Grace",
+                Email = "danielgrace@gmail.com",
+                DateOfBirth = "02/11/2000",
+                CardNumber = "373157862558357",
+                ExpiryDate = "04/24",
+                NameOnCard = "Daniel",
+                SecurityCode = "742",
+                StreetName = "Manor road",
+                City = "Aylesbury",
+                PostCode = "HP55 1JO"
+            };
+            return View(payment);
+        }
+
+        [HttpPost]
+        public IActionResult PaymentDetails(PaymentDetails payment)
+        {
+            //Save to database
+            ViewData["Payment details"] = payment;
+            ViewBag.Payment = payment;
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CO453_WebApplication1/Models/PaymentDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CO453_WebApplication1.Models
+{
+    public class PaymentDetails
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string SecondName { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Date)]
+        public string DateOfBirth { get; set; }
+        [Required, DataType(DataType.CreditCard)]
+        public string CardNumber { get; set; }
+        [Required]
+        public string ExpiryDate { get; set; }
+        [Required]
+        public string NameOnCard { get; set; }
+        [Required]
+        public string SecurityCode { get; set; }
+        [Required]
+        public string StreetName { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required, DataType(DataType.PostalCode)]
+        public string PostCode { get; set; }
+    }
+}
